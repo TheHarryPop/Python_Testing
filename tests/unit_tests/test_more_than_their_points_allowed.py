39b270766c24285d95de66a3def5f494a22693cf
TheHarryPop <winkelquentin@gmail.com>
2022-01-28 10:31:13 +0100

    All fonctions OK - Units tests OK

---
+++ tests/unit_tests/test_more_than_their_points_allowed.py
@@ -0,0 +1,22 @@
+def test_purchase_less_than_their_points_allowed(client):
+    response = client.post('/purchasePlaces', data={'club': 'Iron Temple', 'competition': 'Spring Festival',
+                                                    'places': '3'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Great-booking complete!' in response_data
+
+
+def test_purchase_their_points_allowed(client):
+    response = client.post('/purchasePlaces', data={'club': 'Iron Temple', 'competition': 'Spring Festival',
+                                                    'places': '4'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Great-booking complete!' in response_data
+
+
+def test_purchase_more_than_their_points_allowed(client):
+    response = client.post('/purchasePlaces', data={'club': 'Iron Temple', 'competition': 'Spring Festival',
+                                                    'places': '9'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'ERROR : your points balance' in response_data
