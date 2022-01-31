5b9d3c0fc11654f9e217e7a2a7b4ee9923d038fd
TheHarryPop <winkelquentin@gmail.com>
2022-01-31 12:02:58 +0100

    merge "more_than_12_place"

--- tests/unit_tests/test_more_than_12_places.py
+++ tests/unit_tests/test_more_than_12_places.py
@@ -2,7 +2,7 @@ from tests.utils import mock_competitions_n_clubs
 
 
 def test_single_purchase_less_than_12_places(client, mock_competitions_n_clubs):
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '8'})
     response_data = response.data.decode()
     assert response.status_code == 200
@@ -10,7 +10,7 @@ def test_single_purchase_less_than_12_places(client, mock_competitions_n_clubs):
 
 
 def test_single_purchase_12_places(client, mock_competitions_n_clubs):
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '12'})
     response_data = response.data.decode()
     assert response.status_code == 200
@@ -18,17 +18,17 @@ def test_single_purchase_12_places(client, mock_competitions_n_clubs):
 
 
 def test_single_purchase_more_than_12_places(client, mock_competitions_n_clubs):
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
-                                                    'places': '14'})
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
+                                                    'places': '13'})
     response_data = response.data.decode()
     assert response.status_code == 200
     assert 'ERROR' in response_data
 
 
 def test_multiple_purchase_less_than_12_places(client, mock_competitions_n_clubs):
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '8'})
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '1'})
     response_data = response.data.decode()
     assert response.status_code == 200
@@ -36,9 +36,9 @@ def test_multiple_purchase_less_than_12_places(client, mock_competitions_n_clubs
 
 
 def test_multiple_purchase_12_places(client, mock_competitions_n_clubs):
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '10'})
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '2'})
     response_data = response.data.decode()
     assert response.status_code == 200
@@ -46,9 +46,9 @@ def test_multiple_purchase_12_places(client, mock_competitions_n_clubs):
 
 
 def test_multiple_purchase_more_than_12_places(client, mock_competitions_n_clubs):
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
-                                                    'places': '11'})
-    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
+                                                    'places': '10'})
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Back to the Future',
                                                     'places': '3'})
     response_data = response.data.decode()
     assert response.status_code == 200
