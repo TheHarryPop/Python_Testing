b0e0948b8713e9cae621cc0af91d1f870a04d10b
TheHarryPop <winkelquentin@gmail.com>
2022-01-31 11:56:44 +0100

    add multiple unit tests + mock

--- tests/unit_tests/test_more_than_12_places.py
+++ tests/unit_tests/test_more_than_12_places.py
@@ -1,5 +1,7 @@
+from tests.utils import mock_competitions_n_clubs
 
-def test_purchase_less_than_12_places(client):
+
+def test_single_purchase_less_than_12_places(client, mock_competitions_n_clubs):
     response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
                                                     'places': '8'})
     response_data = response.data.decode()
@@ -7,7 +9,7 @@ def test_purchase_less_than_12_places(client):
     assert 'Great-booking complete!' in response_data
 
 
-def test_purchase_12_places(client):
+def test_single_purchase_12_places(client, mock_competitions_n_clubs):
     response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
                                                     'places': '12'})
     response_data = response.data.decode()
@@ -15,9 +17,39 @@ def test_purchase_12_places(client):
     assert 'Great-booking complete!' in response_data
 
 
-def test_purchase_more_than_12_places(client):
+def test_single_purchase_more_than_12_places(client, mock_competitions_n_clubs):
     response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
                                                     'places': '14'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'ERROR' in response_data
+
+
+def test_multiple_purchase_less_than_12_places(client, mock_competitions_n_clubs):
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                    'places': '8'})
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                    'places': '1'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Great-booking complete!' in response_data
+
+
+def test_multiple_purchase_12_places(client, mock_competitions_n_clubs):
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                    'places': '10'})
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                    'places': '2'})
+    response_data = response.data.decode()
+    assert response.status_code == 200
+    assert 'Great-booking complete!' in response_data
+
+
+def test_multiple_purchase_more_than_12_places(client, mock_competitions_n_clubs):
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                    'places': '11'})
+    response = client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                    'places': '3'})
     response_data = response.data.decode()
     assert response.status_code == 200
     assert 'ERROR' in response_data
