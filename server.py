07fabfc19f4426ae059c204421bc22c43149be45
TheHarryPop <winkelquentin@gmail.com>
2022-02-01 09:14:23 +0100

    fix reduction

--- server.py
+++ server.py
@@ -48,17 +48,10 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    if placesRequired < 13:
-        if placesRequired > int(club['points']):
-            flash('ERROR : your points balance is too low')
-        else:
-            competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-            club['points'] = int(club['points'])-placesRequired
-            flash('Great-booking complete!')
-    else:
-        flash('ERROR : You can only reserve a maximum of 12 places')
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+    club['points'] = int(club['points']) - placesRequired
+    flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
-
 
 # TODO: Add route for points display
 
