2366cec40386d547906ecb748c52c15a15adbc78
TheHarryPop <winkelquentin@gmail.com>
2022-02-01 10:28:30 +0100

    fix locustfile.py

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -13,15 +13,16 @@ class ProjectPerfTest(HttpUser):
 
     @task
     def show_summary(self):
-        self.client.post('/showSummary', {'email': 'kate@shelifts.co.uk'})
+        self.client.post('/showSummary', data={'email': 'kate@shelifts.co.uk'})
 
     @task
     def book(self):
         self.client.get('/book/Spring Festival/She Lifts')
 
     @task
-    def purchase(self):
-        self.client.post('/purchasePlaces', {'club': 'She Lifts', 'competition': 'Spring Festival', 'places': '4'})
+    def purchase_places(self):
+        self.client.post('/purchasePlaces', data={'club': 'She Lifts', 'competition': 'Spring Festival',
+                                                  'places': '4'})
 
     @task
     def logout(self):
