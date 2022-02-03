2ffa0967240c81e6f1cc370407bb7fc986ac1a5a
TheHarryPop <winkelquentin@gmail.com>
2022-02-03 19:13:05 +0100

    add functional tests - selenium

--- tests/functional_tests/test_functional.py
+++ tests/functional_tests/test_functional.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions
 
 url = 'http://127.0.0.1:5000/'
 
@@ -18,7 +18,6 @@ def launch_service():
 def test_display_board():
     driver = launch_service()
     driver.get(url + 'displayBoard')
-    driver.implicitly_wait(5)
     assert driver.find_element(By.ID, 'table')
     driver.quit()
 
@@ -28,7 +27,7 @@ def test_authentication():
     form = driver.find_element(By.NAME, "email")
     form.send_keys("kate@shelifts.co.uk")
     form.submit()
-    WebDriverWait(driver, 5).until(ec.url_to_be(url + 'showSummary'))
+    WebDriverWait(driver, 5).until(expected_conditions.url_to_be(url + 'showSummary'))
     assert driver.find_element(By.LINK_TEXT, 'Logout')
     driver.quit()
 
@@ -38,14 +37,12 @@ def test_booking_places():
     form = driver.find_element(By.NAME, "email")
     form.send_keys("kate@shelifts.co.uk")
     form.submit()
-    driver.implicitly_wait(5)
     link = driver.find_element(By.LINK_TEXT, 'Book Places')
     link.click()
-    driver.implicitly_wait(5)
     form = driver.find_element(By.NAME, 'places')
     form.send_keys("10")
     form.submit()
-    WebDriverWait(driver, 5).until(ec.url_to_be(url + 'purchasePlaces'))
+    WebDriverWait(driver, 5).until(expected_conditions.url_to_be(url + 'purchasePlaces'))
     assert driver.find_element(By.LINK_TEXT, 'Logout')
     driver.quit()
 
